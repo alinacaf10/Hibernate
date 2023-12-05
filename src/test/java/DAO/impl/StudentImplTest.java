1694395e66671359ae05e8e32928d1fe45220526
Ali Nacafov <alinacaf10@gmail.com>
2023-12-05 16:44:13 +0400

    Add tests

---
+++ src/test/java/DAO/impl/StudentImplTest.java
@@ -0,0 +1,34 @@
+package DAO.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findStudentById() {
+    }
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void updateStudent() {
+    }
+
+    @Test
+    void removeStudent() {
+    }
+}
